97cd253a8637b5398b474b543ccbc1ed2a97c399
Guilhem Hermet <guhere@l4712-12.info.polymtl.ca>
2021-04-08 11:37:37 -0400

    ajout connexion et inscription avec recuperation de cles

--- reencryption-api/client.py
+++ reencryption-api/client.py
@@ -3,16 +3,62 @@ import requests
 SERVER_ADDRESS = "127.0.0.1:5000"
 URL = "http://" + SERVER_ADDRESS + "/api/"
 
+def connexion():
+	response = requests.get(URL + "genkey")
+	assert response.status_code == 200
+	data = response.json()
+	assert data["status"] == "ok"
+	alices_private_key = data["privateKey"]
+	alices_public_key = data["publicKey"]
+	
+def inscription():
+	response = requests.get(URL + "getallkeys")
+	assert response.status_code == 200
+	data = response.json()
+	assert data["status"] == "ok"
+	alices_private_key = data["privateKey"]
+	alices_public_key = data["publicKey"]
+
+# Connexion
+choice = 3
+private_key = ''
+public_key = ''
+while(choice != '1'):
+	choice = input('1 - Se connecter\n2 - Creer nouvel utilisateur\nChoix : ')
+
+	if(choice == '1'):
+		username = input('Nom d\'utilisateur : ')
+		## Récupération des clés, attention à ne pas mettre un nom qui n'existe pas pour l'instant
+		response = requests.post(
+			URL + "getallkeys",
+			json={"username": username}
+		)
+		assert response.status_code == 200
+		data = response.json()
+		if(data["status"] == "ok"):
+			private_key = data["privateKey"]
+			public_key = data["publicKey"]
+			print(private_key)
+			print(public_key)
+		else:
+			print(data["error"])
+	if(choice == '2'):
+		username = input('Nom d\'utilisateur : ')
+		response = requests.post(
+			URL + "genuser",
+			json={"username": username}
+		)
+		assert response.status_code == 200
+		data = response.json()
+		if(data["status"] == "ok"):
+			print("Bienvenue " + username)
+		else:
+			print(data["error"])
+
 
 # 1. Generate Alice keys
 
-## Private / public keypair
-response = requests.get(URL + "genkey")
-assert response.status_code == 200
-data = response.json()
-assert data["status"] == "ok"
-alices_private_key = data["privateKey"]
-alices_public_key = data["publicKey"]
+
 
 ## Signing / verifying keypair
 response = requests.get(URL + "genkey")
@@ -38,7 +84,7 @@ plaintext = 'Proxy Re-encryption is cool!'
 print("plaintext: {}".format(plaintext))
 response = requests.post(
     URL + 'encrypt',
-    json={"publicKey": alices_public_key, "plaintext": plaintext}
+    json={"publicKey": public_key, "plaintext": plaintext}
 )
 #print("Request: {}".format(response.text))
 assert response.status_code == 200
@@ -54,7 +100,7 @@ print()
 # 4. Test alice's decryption
 response = requests.post(
     URL + "decrypt",
-    json={"receiverPrivateKey": alices_private_key, "ciphertext": ciphertext, "capsule": alice_capsule}
+    json={"receiverPrivateKey": private_key, "ciphertext": ciphertext, "capsule": alice_capsule}
 )
 assert response.status_code == 200
 data = response.json()
@@ -76,7 +122,7 @@ print()
 # 6. Generate re-encryption key
 response = requests.post(
     URL + "gen_renc_key",
-    json={"delegatorPrivateKey": alices_private_key,
+    json={"delegatorPrivateKey": private_key,
           "delegatorSigningKey": alices_signing_key,
           "receiverPublicKey": bobs_public_key}
 )
@@ -89,7 +135,7 @@ print("Re-encryption key from alice to bob generated")
 # 7. Re-encrypt message
 response = requests.post(
     URL + "re_encrypt",
-    json={"delegatorPublicKey": alices_public_key,
+    json={"delegatorPublicKey": public_key,
           "delegatorVerifyingKey": alices_verifying_key,
           "receiverPublicKey": bobs_public_key,
           "reencKey": alice_to_bob_rencryption_key,
@@ -111,7 +157,7 @@ response = requests.post(
           "capsule": alice_capsule,
           # Re-encryption additional parameters
           "receiverPublicKey": bobs_public_key,
-          "delegatorPublicKey": alices_public_key,
+          "delegatorPublicKey": public_key,
           "delegatorVerifyingKey": alices_verifying_key,
           "cfrag": bob_cfrag}
 )
